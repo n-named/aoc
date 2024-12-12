5a0b8fa9ea44cd0cc1febccc0ff53431f025eba8
no-name <166156272+n-named@users.noreply.github.com>
2024-12-13 00:21:07 +0200

    Add files via upload

---
+++ 2024/day_10/day10_2.py
@@ -0,0 +1,35 @@
+def search(top_map, crd, ends):
+    ncrds = [[crd[0]-1, crd[1]], [crd[0], crd[1]+1], [crd[0]+1, crd[1]], [crd[0], crd[1]-1]]
+    for ncrd in ncrds:
+        if ncrd[0] < 0 or ncrd[1] < 0:
+            continue
+        try:
+            if top_map[ncrd[0]][ncrd[1]] - top_map[crd[0]][crd[1]] == 1:
+                if top_map[ncrd[0]][ncrd[1]] == 9:
+                    ends.append(ncrd.copy())
+                else:
+                    search(top_map, ncrd, ends)
+        except:
+            pass
+
+
+
+with open('input.txt', 'r') as f:
+    input_data = f.read()
+
+top_map = [[int(el) for el in list(line)] for line in input_data.split("\n")]
+
+zeros = []
+for line in range(len(top_map)):
+    for row in range(len(top_map[line])):
+        if top_map[line][row] == 0:
+            zeros.append([line, row])
+
+score = 0
+for crd in zeros:
+    ends = []
+    search(top_map, crd, ends)
+    score = score + len(ends)
+
+print(score)
+    
