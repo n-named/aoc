7f3ac73e3390601c647217d3a3b54c425f6f35bb
no-name <166156272+n-named@users.noreply.github.com>
2024-12-13 23:00:34 +0200

    Update day11_1.py

--- 2024/day_11/day11_1.py
+++ 2024/day_11/day11_1.py
@@ -1,21 +1,21 @@
 with open('input.txt', 'r') as f:
     input_data = f.read()
 
-stones = [int(r) for r in input_data.split(" ")]
+stones = [int(s) for s in input_data.split(" ")]
 
 for clip in range(0, 25):
     stones_t = []
-    for r in range(len(stones)):
-        if stones[r] == 0:
+    for s in range(len(stones)):
+        if stones[s] == 0:
             stones_t.append(1)
 
-        elif len(str(stones[r])) % 2 == 0:
-            r_s = str(stones[r])
-            stones_t.append(int(r_s[:int(len(r_s)/2)]))
-            stones_t.append(int(r_s[int(len(r_s)/2):]))
+        elif len(str(stones[s])) % 2 == 0:
+            s_s = str(stones[s])
+            stones_t.append(int(s_s[:int(len(s_s)/2)]))
+            stones_t.append(int(s_s[int(len(s_s)/2):]))
 
         else:
-            stones_t.append(stones[r]*2024)
+            stones_t.append(stones[s]*2024)
             
     stones = stones_t
 
