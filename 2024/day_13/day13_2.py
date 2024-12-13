32484703c71d7a33fdd5e9963ccbea82e947639d
no-name <166156272+n-named@users.noreply.github.com>
2024-12-13 18:24:09 +0200

    Add files via upload

---
+++ 2024/day_13/day13_2.py
@@ -0,0 +1,31 @@
+import re
+from math import floor
+
+
+with open('input.txt', 'r') as f:
+    input_data = f.read()
+
+input_data = [m.split("\n") for m in input_data.split("\n\n")]
+
+tkns = 0
+for m in input_data:
+    d = {"A": {}, "B": {}, "P": {}}
+    d["A"]["X"] = int(re.findall("X\+(\d{,2})", m[0])[0])
+    d["A"]["Y"] = int(re.findall("Y\+(\d{,2})", m[0])[0])
+
+    d["B"]["X"] = int(re.findall("X\+(\d{,2})", m[1])[0])
+    d["B"]["Y"] = int(re.findall("Y\+(\d{,2})", m[1])[0])
+
+    d["P"]["X"] = int(re.findall("X\=(\d{,5})", m[2])[0]) + 10000000000000
+    d["P"]["Y"] = int(re.findall("Y\=(\d{,5})", m[2])[0]) + 10000000000000
+
+    det = d["A"]["X"]*d["B"]["Y"] - d["B"]["X"]*d["A"]["Y"]
+    A = (d["P"]["X"] * d["B"]["Y"] - d["B"]["X"] * d["P"]["Y"]) / det
+    B = (d["A"]["X"] * d["P"]["Y"] - d["A"]["Y"] * d["P"]["X"]) / det
+    
+    if int(A)/ A != 1 or int(B)/ B != 1:
+        continue
+
+    tkns = tkns + 3*A + B
+
+print(int(tkns))
